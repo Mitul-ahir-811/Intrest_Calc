40dcd2569b3dcad579b798aefdbb522ac2d991e1
Mitul-ahir-811 <2025mca060>
2026-07-06 10:05:37 +0530

    External

---
+++ script.js
@@ -0,0 +1,16 @@
+function calculateSI(){
+
+    let P = parseFloat(document.getElementById("principal").value);
+    let R = parseFloat(document.getElementById("rate").value);
+    let N = parseFloat(document.getElementById("time").value);
+
+    if(isNaN(P)||isNaN(R)||isNaN(N)){
+        document.getElementById("result").innerHTML="Please enter all values";
+        return;
+    }
+
+    let SI = (P*R*N)/100;
+
+    document.getElementById("result").innerHTML =
+    "Simple Interest = " + SI;
+}
